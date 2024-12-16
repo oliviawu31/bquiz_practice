b63559dd6f75992a338c1d58da2570368c45fdcd
oliviawu <oliviawu0301@gmail.com>
2024-12-16 16:25:18 +0800

    完成modal

--- modal/ad.php
+++ modal/ad.php
@@ -1,15 +1,11 @@
 <!-- h3.cent =>文字置中 -->
-<h3 class="cent">新增標題區圖片</h3>
+<h3 class="cent">新增動態文字廣告</h3>
 <hr>
 <!-- 上傳檔案功能=> -->
-<form action="api/insert_title.php" method="post" enctype="multipart/form-data">
+<form action="api/insert_ad.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
-            <td>標題區圖片：</td>
-            <td><input type="file" name="img" id="img"></td>
-        </tr>
-        <tr>
-            <td>標題區替代文字：</td>
+            <td>新增動態文字廣告：</td>
             <td><input type="text" name="text" id="text"></td>
         </tr>
     </table>
--- modal/mvim.php
+++ modal/mvim.php
@@ -1,16 +1,12 @@
 <!-- h3.cent =>文字置中 -->
-<h3 class="cent">新增標題區圖片</h3>
+<h3 class="cent">新增動畫圖片</h3>
 <hr>
 <!-- 上傳檔案功能=> -->
-<form action="api/insert_title.php" method="post" enctype="multipart/form-data">
+<form action="api/insert_mvim.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
-            <td>標題區圖片：</td>
+            <td>動畫圖片：</td>
             <td><input type="file" name="img" id="img"></td>
-        </tr>
-        <tr>
-            <td>標題區替代文字：</td>
-            <td><input type="text" name="text" id="text"></td>
         </tr>
     </table>
     <div class="cent"><input type="submit" value="新增">
